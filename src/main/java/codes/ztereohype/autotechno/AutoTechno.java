929f9841223fcc9444f05c69dc36f6fcd20e6b17
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2024-05-15 20:50:49 -0400

    few tweaks

--- src/main/java/codes/ztereohype/autotechno/AutoTechno.java
+++ src/main/java/codes/ztereohype/autotechno/AutoTechno.java
@@ -4,18 +4,18 @@ import codes.ztereohype.autotechno.chat.EventDetector;
 import codes.ztereohype.autotechno.chat.MessageRandomiser;
 import codes.ztereohype.autotechno.client.ClientWrapper;
 import codes.ztereohype.autotechno.config.AutoTechnoConfig;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class AutoTechno implements ModInitializer {
+public class AutoTechno implements ClientModInitializer {
     public static final Logger LOGGER = LogManager.getLogger();
     public static ClientWrapper client;
     public static EventDetector detector;
     public static MessageRandomiser messageRandomiser;
 
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         AutoTechnoConfig.init();
 
         client = new ClientWrapper();
--- src/main/java/codes/ztereohype/autotechno/mixin/MixinClientConnection.java
+++ src/main/java/codes/ztereohype/autotechno/mixin/MixinClientConnection.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class MixinClientConnection {
-    @Inject(method = "disconnect", at = @At("HEAD"))
+    @Inject(method = "disconnect", at = @At("HEAD"), require = 0)
     private void onDisconnect(Text disconnectReason, CallbackInfo ci) {
         if (AutoTechno.client.getCurrentServer() == Server.MINEPLEX) {
             EventDetector.mineplexStart = false;
